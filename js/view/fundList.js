214a3a2a74cfa492d04e38c530b7a88a83933c40
Conrado Quilles Gomes <conradoqg@gmail.com>
2018-09-26 18:18:44 -0300

    Add ordering to fund list

--- js/view/fundList.js
+++ js/view/fundList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import Divider from '@material-ui/core/Divider';
@@ -8,6 +9,10 @@ import TablePagination from '@material-ui/core/TablePagination';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ReorderIcon from '@material-ui/icons/Reorder';
 
 const styles = theme => ({
     heading: {
@@ -17,22 +22,45 @@ const styles = theme => ({
 
 const formatters = {
     somethingToPercentage: (value) => !isNaN(value) ? (value * 100).toFixed(2) : null,
-    somethingToValue: (value) => !isNaN(value) ? value.toFixed(2) : null,
+    somethingToValue: (value) => value != null && !isNaN(value)  ? value.toFixed(2) : null,
     aValueOrTrace: (value) => value == null ? '-' : value
 };
+
+const sortOptions = [
+    {
+        displayName: 'CNPJ',
+        field: 'icf_cnpj_fundo',
+        order: 'asc'
+    },
+    {
+        displayName: 'Desempenho 1 Ano (Asc)',
+        field: 'iry_investment_return_1y',
+        order: 'asc'
+    },
+    {
+        displayName: 'Desempenho 1 Ano (Desc)',
+        field: 'iry_investment_return_1y',
+        order: 'desc'
+    }
+];
+
+const ITEM_HEIGHT = 48;
 
 class FundListView extends React.Component {
     state = {
         data: [],
         page: 0,
         count: 0,
-        rowsPerPage: 5
+        rowsPerPage: 5,
+        anchorEl: null,
+        sort: sortOptions[0]
     };
 
     handleChangePage = async (object, page) => {
         const result = await this.getData({
             page,
-            rowsPerPage: this.state.rowsPerPage
+            rowsPerPage: this.state.rowsPerPage,
+            sort: this.state.sort
         });
         this.setState({
             page,
@@ -44,7 +72,8 @@ class FundListView extends React.Component {
     handleChangeRowsPerPage = async (event) => {
         const result = await this.getData({
             page: this.state.page,
-            rowsPerPage: event.target.value
+            rowsPerPage: event.target.value,
+            sort: this.state.sort
         });
         this.setState({
             rowsPerPage: event.target.value,
@@ -53,9 +82,31 @@ class FundListView extends React.Component {
         });
     }
 
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
+    handleOrderMenuItemClick = async (event, index) => {
+        const result = await this.getData({
+            page: this.state.page,
+            rowsPerPage: this.state.rowsPerPage,
+            sort: sortOptions[index]
+        });
+        this.setState({
+            sort: sortOptions[index],
+            anchorEl: null,
+            data: result.data
+        });
+    }
+
     async getData(options) {
         const range = options.page * options.rowsPerPage + '-' + ((options.page * options.rowsPerPage) + options.rowsPerPage);
-        const fundListObject = await fetch('http://localhost:82/inf_cadastral_fi_with_xpi_and_iryf_of_last_year', {
+        const sort = options.sort.field + '.' + options.sort.order;
+        const fundListObject = await fetch('http://localhost:82/inf_cadastral_fi_with_xpi_and_iryf_of_last_year?order=' + sort, {
             method: 'GET',
             headers: {
                 'Range-Unit': 'items',
@@ -85,6 +136,8 @@ class FundListView extends React.Component {
 
     render() {
         const { classes } = this.props;
+        const { anchorEl } = this.state;
+        const open = Boolean(anchorEl);
 
         return (
             <div>
@@ -92,6 +145,38 @@ class FundListView extends React.Component {
                 <Typography variant="display1" gutterBottom>Lista de Fundos</Typography>
                 <Grid container spacing={24}>
                     <Grid item xs={8}>
+                        <Paper elevation='1' square='true'>
+                            <Grid
+                                container
+                                direction="row"
+                                justify="flex-end"
+                                alignItems="center">
+                                <IconButton
+                                    aria-label="More"
+                                    aria-owns={open ? 'long-menu' : null}
+                                    aria-haspopup="true"
+                                    onClick={this.handleClick}>
+                                    <ReorderIcon />
+                                </IconButton>
+                                <Menu
+                                    id="long-menu"
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={this.handleClose}
+                                    PaperProps={{
+                                        style: {
+                                            maxHeight: ITEM_HEIGHT * 4.5,
+                                            width: 220,
+                                        },
+                                    }}>
+                                    {sortOptions.map((option, index) => (
+                                        <MenuItem key={option.displayName} selected={option.displayName === this.state.sort.displayName} onClick={event => this.handleOrderMenuItemClick(event, index)}>
+                                            {option.displayName}
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </Grid>
+                        </Paper>
                         {this.state.data.map((fund, index) => (
                             <ExpansionPanel key={index}>
                                 <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
@@ -153,7 +238,6 @@ class FundListView extends React.Component {
                                 </ExpansionPanelDetails>
                             </ExpansionPanel>
                         ))}
-
                         <TablePagination
                             component="div"
                             count={this.state.count}
