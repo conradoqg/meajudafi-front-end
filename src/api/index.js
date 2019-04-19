21a1f8f73c7b55044c311ad36562c31635348a3e
Conrado Quilles Gomes <conradoqg@gmail.com>
2019-04-19 11:05:28 -0300

    Do not rely on env variable to set the API url

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import allKeys from 'promise-results/allKeys';
 import packageJson from '../../package.json';
 
 /* global process */
-const API_URL = process.env.API_URL || `api.${window.location.host}`;
+const API_URL = `api.${window.location.host}`;
 
 export default {
     getFundList: async (options, fromDate = new Date((new Date()).getFullYear(), 0, 1)) => {
