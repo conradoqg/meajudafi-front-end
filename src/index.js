1a0b6721b68b20a7998f2e3ad2f0eef31ebeab38
Conrado Quilles Gomes <conradoqg@gmail.com>
2019-04-24 16:31:36 -0300

    Add a basic serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DashboardView from './view/dashboardView';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<DashboardView />, document.getElementById('app'));
+
+serviceWorker.register();
