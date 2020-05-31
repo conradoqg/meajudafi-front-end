0eab04809d770e2a8a0fa50b3b30971def1297b2
Conrado Quilles Gomes <conradoqg@gmail.com>
2020-05-30 22:52:05 -0300

    Change the verification method of maintenance mode

--- src/api/isInMaintenanceMode.js
+++ src/api/isInMaintenanceMode.js
@@ -3,14 +3,19 @@ import { PROTOCOL, API_URL } from './index';
 
 export default async () => {
     const currentVersionArray = packageJson.version.split('.').map(value => parseInt(value));
-    const minor = currentVersionArray[currentVersionArray.length - 2];
-    const lastMigrationObject = await fetch(`${PROTOCOL}//${API_URL}/migrations?order=name.desc&limit=1`);
-    if (lastMigrationObject.status < 200 || lastMigrationObject.status > 299)
+    const minor = parseInt(currentVersionArray[currentVersionArray.length - 2]);
+
+    const workerVersionObject = await fetch(`${PROTOCOL}//${API_URL}/about?select=info->>version&what=eq.worker`);
+
+    if (workerVersionObject.status < 200 || workerVersionObject.status > 299)
         throw new Error('Unable to retrieve maintenance mode');
-    const lastMigrationData = await lastMigrationObject.json();
-    let migrationMinor = 0;
-    if (lastMigrationData.length > 0) {
-        migrationMinor = parseInt(lastMigrationData[0].name.substring(0, 8));
+    const workerVersion = await workerVersionObject.json();
+    let workerMinor = 0;
+    if (workerVersion.length > 0) {
+        const workerVersionArray = workerVersion[0].version.split('.').map(value => parseInt(value));
+        workerMinor = parseInt(workerVersionArray[workerVersionArray.length - 2]);
     }
-    return minor !== migrationMinor;
+
+    console.log(`Minor: ${minor} - Worker: ${workerMinor}`);
+    return minor < workerMinor;
 };
