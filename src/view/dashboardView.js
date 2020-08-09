59f2d2cfdde9c4aa805ba77802b217d9c62b3519
Conrado Quilles Gomes <conradoqg@gmail.com>
2020-08-09 11:09:19 -0300

    Move dashboardView to react hooks

--- src/view/dashboardView.js
+++ src/view/dashboardView.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -12,7 +11,7 @@ import ShowChartIcon from '@material-ui/icons/ShowChart';
 import TableChartIcon from '@material-ui/icons/TableChart';
 import ScatterPlotIcon from '@material-ui/icons/ScatterPlot';
 import GithubIcon from 'mdi-material-ui/Github';
-import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
 import IndicatorsView from './indicatorsView';
@@ -24,7 +23,7 @@ import ProgressView from './progressView';
 import API from '../api';
 import GA from '../util/googleAnalytics';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
     },
@@ -58,12 +57,12 @@ const styles = theme => ({
         color: 'white',
         borderBottom: '2px solid'
     }
-});
+}));
 
-const routes = [    
+const routes = [
     {
         path: ['/funds/:cnpj/:benchmark?/:range?/:field?', '/funds/:cnpj'],
-        name: 'Informações sobre o fundo',        
+        name: 'Informações sobre o fundo',
         showInMenu: false,
         icon: () => (<TableChartIcon />),
         widthUp: 'xs',
@@ -128,90 +127,87 @@ const MenuLink = ({ label, to, activeOnlyWhenExact, classes }) => (
     />
 );
 
-class Dashboard extends React.Component {
-    state = {
-        maintenanceMode: false
-    };
+function Dashboard(props) {
+    const [isInMaintenanceMode, setIsInMaintenanceMode] = useState(false);
 
-    componentDidMount = async () => {
-        this.setState({ maintenanceMode: await API.isInMaintenanceMode() });
-    };
+    useEffect(() => {
+        async function updateData() {
+            setIsInMaintenanceMode(await API.isInMaintenanceMode());
+        }
+        updateData();
+    }, []);
 
-    render() {
-        const { classes } = this.props;
+    const classes = useStyles();
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const isWidthUp = breakpoint => useMediaQuery(theme => theme.breakpoints.up(breakpoint));
 
-        if (this.state.maintenanceMode)
-            return (
-                <div className={classes.centered}>
-                    <Typography variant="h6" component="span">
-                        <img src="/img/emoticon-cry-outline.png" className={classes.centeredImage} alt="saddiness" />
-                        Estamos em manutenção, volte em alguns minutos.
-                </Typography>
-                </div>
-            );
-        else return (
-            <Router>
-                {GA.init() && <GA.RouteTracker />}
-                <React.Fragment>
-                    <CssBaseline />
-                    <div className={classes.root}>
-                        <AppBar
-                            position="absolute">
-                            <Toolbar>
-                                <Typography variant="h5" color="inherit" noWrap className={classes.toolbarTitle}>
-                                    Me Ajuda FI
-                                </Typography>
-                                {routes.filter(route => route.showInMenu).sort((routeA, routeB) => routeA.order - routeB.order).filter(route => isWidthUp(route.widthUp, this.props.width)).map((route, index) => (
-                                    <MenuLink activeOnlyWhenExact={route.exact} to={route.linkTo ? route.linkTo : route.path} classes={classes} label={route.name} icon={route.icon} key={index} />
-                                ))}
-                                <Hidden smDown>
-                                    <IconButton color="inherit" aria-label="Repositório no Github" href="https://github.com/conradoqg/cvm-fund-explorer-stack" target="_new" rel="noopener">
-                                        <GithubIcon fontSize="default" />
-                                    </IconButton>
-                                </Hidden>
-                            </Toolbar>
-                        </AppBar>
-                        <main className={classes.content}>
-                            <Switch>
-                                {routes.filter(route => isWidthUp(route.widthUp, this.props.width)).map((route, index) => (
-                                    <Route
-                                        key={index}
-                                        path={route.path}
-                                        exact={route.exact}
-                                        render={props => route.main(props, classes)}
-                                    />
-                                ))}
-                                <Route component={() => (
-                                    <div>
-                                        <div className={classes.appBarSpacer} />
-                                        <Grid container spacing={2}>
-                                            <Grid item xs={12}>
-                                                <Typography variant="h6" align="center" noWrap>Página não encontrada.</Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </div>)} />
-                            </Switch>
-                            <Grid container spacing={2}>
-                                <Grid item xs={12}>
-                                    <Typography variant="caption" gutterBottom>
-                                        <p>Rentabilidade passada não representa garantia de rentabilidade futura.</p>
-                                        <p>A rentabilidade divulgada não é líquida de impostos.</p>
-                                        <p>Fundos de investimento não contam com garantia do administrador, do gestor, de qualquer mecanismo de seguro ou fundo garantidor de crédito – FGC.</p>
-                                        <p>Alguns fundos tem menos de 12 (doze) meses. Para avaliação da performance de um fundo de investimento, é recomendável a análise de, no mínimo, 12 (doze) meses.</p>
-                                        <p>Os dados são extraídos do site da CVM <Link to='/progress'>diariamente</Link> e podem conter erros.</p>
-                                    </Typography>
-                                </Grid>
-                            </Grid>
-                        </main>
-                    </div>
-                </React.Fragment>
-            </Router>
+    if (isInMaintenanceMode)
+        return (
+            <div className={classes.centered}>
+                <Typography variant="h6" component="span">
+                    <img src="/img/emoticon-cry-outline.png" className={classes.centeredImage} alt="saddiness" />
+                    Estamos em manutenção, volte em alguns minutos.
+            </Typography>
+            </div>
         );
-    }
+    else return (
+        <Router>
+            {GA.init() && <GA.RouteTracker />}
+            <React.Fragment>
+                <CssBaseline />
+                <div className={classes.root}>
+                    <AppBar
+                        position="absolute">
+                        <Toolbar>
+                            <Typography variant="h5" color="inherit" noWrap className={classes.toolbarTitle}>
+                                Me Ajuda FI
+                            </Typography>
+                            {routes.filter(route => route.showInMenu).sort((routeA, routeB) => routeA.order - routeB.order).filter(route => isWidthUp(route.widthUp, props.width)).map((route, index) => (
+                                <MenuLink activeOnlyWhenExact={route.exact} to={route.linkTo ? route.linkTo : route.path} classes={classes} label={route.name} icon={route.icon} key={index} />
+                            ))}
+                            <Hidden smDown>
+                                <IconButton color="inherit" aria-label="Repositório no Github" href="https://github.com/conradoqg/meajudafi-stack" target="_new" rel="noopener">
+                                    <GithubIcon fontSize="default" />
+                                </IconButton>
+                            </Hidden>
+                        </Toolbar>
+                    </AppBar>
+                    <main className={classes.content}>
+                        <Switch>
+                            {routes.filter(route => isWidthUp(route.widthUp, props.width)).map((route, index) => (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    exact={route.exact}
+                                    render={props => route.main(props, classes)}
+                                />
+                            ))}
+                            <Route component={() => (
+                                <div>
+                                    <div className={classes.appBarSpacer} />
+                                    <Grid container spacing={2}>
+                                        <Grid item xs={12}>
+                                            <Typography variant="h6" align="center" noWrap>Página não encontrada.</Typography>
+                                        </Grid>
+                                    </Grid>
+                                </div>)} />
+                        </Switch>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Typography variant="caption" gutterBottom>
+                                    <p>Rentabilidade passada não representa garantia de rentabilidade futura.</p>
+                                    <p>A rentabilidade divulgada não é líquida de impostos.</p>
+                                    <p>Fundos de investimento não contam com garantia do administrador, do gestor, de qualquer mecanismo de seguro ou fundo garantidor de crédito – FGC.</p>
+                                    <p>Alguns fundos tem menos de 12 (doze) meses. Para avaliação da performance de um fundo de investimento, é recomendável a análise de, no mínimo, 12 (doze) meses.</p>
+                                    <p>Os dados são extraídos do site da CVM <Link to='/progress'>diariamente</Link> e podem conter erros.</p>
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </main>
+                </div>
+            </React.Fragment>
+        </Router>
+    );
 }
 
-Dashboard.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withWidth()(withStyles(styles)(Dashboard));
+export default Dashboard;
